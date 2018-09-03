9467f9de606b3c856b7594a5047bab9bf3c60f50
Nick Romans <nicholas.romans@gmail.com>
2018-09-03 21:24:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,7 @@ def line(array)
 end
 end
 
+<<<<<<< HEAD
 def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
@@ -24,4 +25,8 @@ def now_serving(katz_deli)
 else
   puts "There is nobody waiting to be served!"
 end
+=======
+def take_a_number(katz_deli, new_to_line)
+ puts "Welcome #{new_to_line}.  You are #{katz_deli.length + 1} in line."
+>>>>>>> 5f27a85c24eb15753407ece3cebaa368dc548dde
 end
